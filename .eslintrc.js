6fb3065ecfb362d3444590c78a216db8948a1c42
Alex Kaidan <akaidan@daydreamapts.com>
2021-07-21 12:54:19 -0600

    Lint, prettier, and ts checking working

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    "@typescript-eslint/no-unused-vars": ["error", {
+      argsIgnorePattern: "^_"
+    }]
+  }
+}
